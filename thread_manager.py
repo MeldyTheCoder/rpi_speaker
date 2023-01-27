78ea7a825eca21e8032ffa30cbea1c2e2a6aab94
Meldy <53063665+MeldyTheCoder@users.noreply.github.com>
2023-01-27 20:55:59 +0300

    Add files via upload

--- thread_manager.py
+++ thread_manager.py
@@ -11,12 +11,10 @@ class ThreadManager:
         def wrapper(*args, **kwargs):
             thread_id = start_new_thread(func, args, kwargs)
             self.threads.append(thread_id)
-            return thread_id
         return wrapper
 
     def async_thread(self, func):
         def wrapper(*args, **kwargs):
             self.loop.create_task(func, name=func.__name__)
             self.async_threads.append(func.__name__)
-            return func.__name__
         return wrapper
