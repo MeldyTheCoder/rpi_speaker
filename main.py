0ca3c5f197286b382181c06b7fa0c6c87cb6f8a8
Meldy <53063665+MeldyTheCoder@users.noreply.github.com>
2023-01-27 21:10:22 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from speak import Speaker
+import time
 
 sp = Speaker()
 
@@ -6,12 +7,15 @@ sp = Speaker()
 def task1():
     for i in range(0, 20):
         print(i)
+    time.sleep(5)
+
 
 @sp.sound
 def task2():
     for i in range(10, 60):
         if i == 59:
             raise KeyError
+
 
 
 task1()
